787d9dde0a300a2b43fb436b0320c0b2944b29e0
periklis92 <periss92@hotmail.com>
2023-12-25 13:34:13 +0200

    Rebuilt project

---
+++ docs/app/immutable/chunks/paths.4yHiVIUI.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1exr0r9?.base??"/focus.lang",e=globalThis.__sveltekit_1exr0r9?.assets??s;export{e as a,s as b};
--- docs/app/immutable/chunks/paths.OtId6lHj.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_1epdn4m?.base??"/focus.lang",e=globalThis.__sveltekit_1epdn4m?.assets??s;export{e as a,s as b};
