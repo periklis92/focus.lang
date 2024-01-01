9a5154beb1d13f47b38075e7f72a93ae046f0148
periklis92 <periss92@hotmail.com>
2024-01-01 14:15:38 +0200

    rebuilt playground

---
+++ docs/app/immutable/chunks/paths.-nX7b_SP.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_jly37f?.base??"/focus.lang",a=globalThis.__sveltekit_jly37f?.assets??s;export{a,s as b};
--- docs/app/immutable/chunks/paths.WeX6Jq83.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_w4ckih?.base??"/focus.lang",a=globalThis.__sveltekit_w4ckih?.assets??s;export{a,s as b};
